ad0d721c46e0c76aa94528359357d3b9810a873e
Champagne <33033657+Laz88@users.noreply.github.com>
2020-05-14 23:18:45 -0500

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+##########################
+# README FOR NEW USERS   #
+##########################
+#
+#	Run this command:
+# 	bash <(wget -O - https://raw.githubusercontent.com/Laz88/swapfile/master/s.sh)
+#
+##########################
+# SCRIPT BEGINS HERE     #
+##########################
+
 sudo dd if=/dev/zero of=/swapfile bs=128M count=32
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
